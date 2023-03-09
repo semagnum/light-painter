a1be88810a335e7b022648794f5b7a8c1a68471f
Spencer Magnusson <semagnum@gmail.com>
2023-03-09 15:02:31 -0500

    Reflect to camera axis option

--- input/axis.py
+++ input/axis.py
@@ -1,8 +1,14 @@
 import bpy
 from mathutils import Vector
 
+VECTORS = {'X': Vector((1, 0, 0)), 'Y': Vector((0, 1, 0)), 'Z': Vector((0, 0, 1))}
 RAY_OFFSET = 0.001
 MAX_RAY_DISTANCE = 0.1
+
+
+def reflect_vector(input_vector, normal):
+    dn = 2 * input_vector.dot(normal)
+    return input_vector - normal * dn
 
 
 def axis_prop():
@@ -14,19 +20,16 @@ def axis_prop():
             ('Y', 'Y', "", 2),
             ('Z', 'Z', "", 3),
             ('NORMAL', 'Normal', 'Along annotation\'s vertex normal', 4),
-            ('NORMAL-RAY', 'Normal + Surface', 'Casts rays to approximate normal of underlying surface', 5),
+            ('NORMAL-RAY', 'Normal + Surface', 'Casts rays to estimate normal of underlying surface', 5),
+            ('REFLECT', 'Reflect to camera', 'Casts rays from camera to determine offset, best for reflections', 6),
         ],
         default='NORMAL-RAY'
     )
 
 
 def offset_points(context, vertices, normals, axis_val: str, offset_amount: float) -> Vector:
-    if axis_val == 'X':
-        vertices = [v + Vector((1, 0, 0)) * offset_amount for v in vertices]
-    elif axis_val == 'Y':
-        vertices = [v + Vector((0, 1, 0)) * offset_amount for v in vertices]
-    elif axis_val == 'Z':
-        vertices = [v + Vector((0, 0, 1)) * offset_amount for v in vertices]
+    if axis_val in VECTORS:
+        vertices = [v + VECTORS[axis_val] * offset_amount for v in vertices]
 
     elif axis_val == 'NORMAL':
         vertices = [v + n * offset_amount for v, n in zip(vertices, normals)]
@@ -40,6 +43,21 @@ def offset_points(context, vertices, normals, axis_val: str, offset_amount: floa
                                                                            distance=MAX_RAY_DISTANCE)
             if is_hit:
                 normals[idx] = hit_normal
+
+        vertices = [v + n * offset_amount for v, n in zip(vertices, normals)]
+    elif axis_val == 'REFLECT':
+        scene = context.scene
+        camera = scene.camera
+        camera_origin = camera.matrix_world.translation
+
+        scene = context.scene
+        depsgraph = context.evaluated_depsgraph_get()
+        for idx, v, n in zip(range(len(vertices)), vertices, normals):
+            direction = v - camera_origin
+            direction.normalized()
+            is_hit, _loc, hit_normal, _idx, _obj, _matrix = scene.ray_cast(depsgraph, camera_origin, direction)
+            if is_hit:
+                normals[idx] = reflect_vector(direction, hit_normal)
 
         vertices = [v + n * offset_amount for v, n in zip(vertices, normals)]
 
