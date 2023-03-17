bd5a9ff5cd9902540f0088831d2eb70527516039
Spencer Magnusson <semagnum@gmail.com>
2023-03-17 09:30:39 -0400

    Fix missing import

--- operators/__init__.py
+++ operators/__init__.py
@@ -1,5 +1,4 @@
 from .ConvexLight import LP_OT_ConvexLight
-from .ConvexFlag import LP_OT_ConvexFlag
 from .Skin import LP_OT_Skin
 from .AreaLight import LP_OT_AreaLight
 from .PointLight import LP_OT_PointLight
