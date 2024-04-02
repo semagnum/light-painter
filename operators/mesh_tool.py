40c10201c1716b7acc6c089fc0c893401e28b184
CP-Design <107256886+chenpaner@users.noreply.github.com>
2024-04-02 17:49:23 +0800

    Add files via upload

--- operators/mesh_tool.py
+++ operators/mesh_tool.py
@@ -7,6 +7,11 @@ from .visibility import VisibilitySettings
 from ..axis import prep_stroke
 from ..keymap import is_event_command, UNIVERSAL_COMMAND_STR as UCS
 
+if bpy.app.version >= (4, 1):
+    from bpy.app.translations import pgettext_rpt as rpt_
+else:
+    from bpy.app.translations import pgettext_tip as rpt_
+
 EMISSIVE_MAT_NAME = 'LightPaint_Emissive'
 MESH_DATA_NAME = 'LightPaint_Convex'
 TUBE_DATA_NAME = 'LightPaint_Tube'
@@ -112,33 +117,61 @@ class LIGHTPAINTER_OT_Mesh(bpy.types.Operator, BaseLightPaintTool, VisibilitySet
 
         return True
 
+    # def get_header_text(self):
+    #     if self.drag_attr == 'offset':
+    #         return 'Offset: {}'.format(
+    #             convert_val_to_unit_str(self.offset, 'LENGTH') + get_drag_mode_header()
+    #         )
+    #     elif self.drag_attr == 'emit_value':
+    #         return 'Power: {}'.format(self.emit_value) + get_drag_mode_header()
+
+    #     return super().get_header_text() + (
+    #         '{}: flatten ({}), '
+    #         '{}: offset mode, '
+    #         '{}: power mode, '
+    #         '{}{}{}{}: axis ({}), '
+    #         '{}: Camera ({}), '
+    #         '{}: Diffuse ({}), '
+    #         '{}: Specular ({}), '
+    #         '{}: Volume ({})'
+    #     ).format(
+    #         UCS['FLATTEN_TOGGLE'], 'ON' if self.flatten else 'OFF',
+    #         UCS['OFFSET_MODE'],
+    #         UCS['POWER_MODE'],
+    #         UCS['AXIS_X'], UCS['AXIS_Y'], UCS['AXIS_Z'], UCS['AXIS_REFLECT'], self.axis,
+    #         UCS['VISIBILITY_TOGGLE_CAMERA'], 'ON' if self.visible_camera else 'OFF',
+    #         UCS['VISIBILITY_TOGGLE_DIFFUSE'], 'ON' if self.visible_diffuse else 'OFF',
+    #         UCS['VISIBILITY_TOGGLE_SPECULAR'], 'ON' if self.visible_specular else 'OFF',
+    #         UCS['VISIBILITY_TOGGLE_VOLUME'], 'ON' if self.visible_volume else 'OFF',
+    #     )
     def get_header_text(self):
         if self.drag_attr == 'offset':
-            return 'Offset: {}'.format(
+            return rpt_('Offset: {}').format(
                 convert_val_to_unit_str(self.offset, 'LENGTH') + get_drag_mode_header()
             )
         elif self.drag_attr == 'emit_value':
-            return 'Power: {}'.format(self.emit_value) + get_drag_mode_header()
+            return rpt_('Power: {}').format(self.emit_value) + get_drag_mode_header()
 
         return super().get_header_text() + (
-            '{}: flatten ({}), '
-            '{}: offset mode, '
-            '{}: power mode, '
-            '{}{}{}{}: axis ({}), '
-            '{}: Camera ({}), '
-            '{}: Diffuse ({}), '
-            '{}: Specular ({}), '
-            '{}: Volume ({})'
+            '{}: {} ({}), '
+            '{}: {}, '
+            '{}: {}, '
+            '{}{}{}{}: {} ({}), '
+            '{}: {}, ({})'  # Camera mode, visibility status
+            '{}: {}, ({})'  # Diffuse mode, visibility status
+            '{}: {}, ({})'  # Specular mode, visibility status
+            '{}: {}, ({})'  # Volume mode, visibility status
         ).format(
-            UCS['FLATTEN_TOGGLE'], 'ON' if self.flatten else 'OFF',
-            UCS['OFFSET_MODE'],
-            UCS['POWER_MODE'],
-            UCS['AXIS_X'], UCS['AXIS_Y'], UCS['AXIS_Z'], UCS['AXIS_REFLECT'], self.axis,
-            UCS['VISIBILITY_TOGGLE_CAMERA'], 'ON' if self.visible_camera else 'OFF',
-            UCS['VISIBILITY_TOGGLE_DIFFUSE'], 'ON' if self.visible_diffuse else 'OFF',
-            UCS['VISIBILITY_TOGGLE_SPECULAR'], 'ON' if self.visible_specular else 'OFF',
-            UCS['VISIBILITY_TOGGLE_VOLUME'], 'ON' if self.visible_volume else 'OFF',
+            UCS['FLATTEN_TOGGLE'], rpt_('flatten'), 'ON' if self.flatten else 'OFF',
+            UCS['OFFSET_MODE'],rpt_('offset mode'),
+            UCS['POWER_MODE'],rpt_('power mode'),
+            UCS['AXIS_X'], UCS['AXIS_Y'], UCS['AXIS_Z'], UCS['AXIS_REFLECT'],rpt_('axis'), self.axis,
+            UCS['VISIBILITY_TOGGLE_CAMERA'], rpt_('Camera'), rpt_('ON' if self.visible_camera else 'OFF'),
+            UCS['VISIBILITY_TOGGLE_DIFFUSE'], rpt_('Diffuse'), rpt_('ON' if self.visible_diffuse else 'OFF'),
+            UCS['VISIBILITY_TOGGLE_SPECULAR'], rpt_('Specular'), rpt_('ON' if self.visible_specular else 'OFF'),
+            UCS['VISIBILITY_TOGGLE_VOLUME'], rpt_('Volume'), rpt_('ON' if self.visible_volume else 'OFF'),
         )
+
 
     @staticmethod
     def generate_mesh(vertices, normals, flatten: bool):
---
+++ translations/__init__.py
@@ -0,0 +1,61 @@
+import bpy
+import json
+import os
+import importlib
+
+####记得要在插件的总__init__.py文件里添加下面几行翻译才有用
+# from . import translations
+# def register():
+#     translations.register()
+# def unregister():
+#     translations.unregister()
+####
+
+class TranslationHelper():
+    def __init__(self, name: str, data: dict, lang='zh_CN'):
+        self.name = name
+        self.translations_dict = dict()
+
+        for src, src_trans in data.items():
+            key = ("Operator", src)
+            self.translations_dict.setdefault(lang, {})[key] = src_trans
+            key = ("*", src)
+            self.translations_dict.setdefault(lang, {})[key] = src_trans
+
+    def register(self):
+        try:
+            bpy.app.translations.register(self.name, self.translations_dict)
+        except(ValueError):
+            pass
+
+    def unregister(self):
+        bpy.app.translations.unregister(self.name)
+
+
+# Set
+############
+from . import zh_CN, ja_JP
+
+LightPainter_zh_CN = TranslationHelper('LightPainter_zh_CN', zh_CN.data)
+LightPainter_zh_HANS = TranslationHelper('LightPainter_zh_HANS', zh_CN.data, lang='zh_HANS')
+LightPainter_ja_JP = TranslationHelper('LightPainter_ja_JP', ja_JP.data, lang='ja_JP')
+
+
+def register():
+    LightPainter_ja_JP.register()
+
+    if bpy.app.version < (4, 0, 0):
+        LightPainter_zh_CN.register()
+    else:
+        LightPainter_zh_CN.register()
+        LightPainter_zh_HANS.register()
+
+
+def unregister():
+    LightPainter_ja_JP.unregister()
+
+    if bpy.app.version < (4, 0, 0):
+        LightPainter_zh_CN.unregister()
+    else:
+        LightPainter_zh_CN.unregister()
+        LightPainter_zh_HANS.unregister()
